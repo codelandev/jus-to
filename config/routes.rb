899fae0e1a4f1875b8415160618b5b981b0713c3
anaschwendler <schwendler@gmail.com>
2016-08-22 18:20:31 -0300

    Starts about section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  get :sobre, to: 'pages#about', as: :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
