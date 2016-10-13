9c9f2a302a38fba502285d6316dd071c910ff6fc
André Pinho <andresouzapinho@gmail.com>
2016-10-13 20:28:31 -0300

    Add basic spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it_behaves_like "a user"
+
+  describe 'factory' do
+    it { expect(build(:user)).to be_valid }
+  end
 end
