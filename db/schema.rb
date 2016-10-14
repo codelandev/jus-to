0ce96c338f3e35f930ff730f90623ffa9cee47a1
André Pinho <andresouzapinho@gmail.com>
2016-10-14 00:05:57 -0300

    Add Agent relation to Legal Case

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013231603) do
+ActiveRecord::Schema.define(version: 20161013233513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20161013231603) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "agent_id"
   end
 
   add_index "legal_cases", ["user_id"], name: "index_legal_cases_on_user_id", using: :btree
